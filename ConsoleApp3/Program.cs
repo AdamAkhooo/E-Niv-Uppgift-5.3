e9032ab137c52b3a310423b3e433616acb009cbe
Admin <Admin@TEBL123005>
2023-01-25 12:03:06 +0100

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("skriv tre heltal");
+
+        string[] tal = new string[3];
+        tal[0] = Console.ReadLine();
+        tal[1] = Console.ReadLine();
+
+        tal[2] = Console.ReadLine();
+
+        Console.WriteLine("Här är de tre Talen");
+        Console.WriteLine($"{tal[0]}, {tal[1]} och {tal[2]}");
+    }
+}
